8f10b70f78634952d61c4bfec98640e800a24e2d
Raza1361 <muhammadraza.nat@gmail.com>
2025-01-14 21:43:00 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySideNavbar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySideNavbar").style.width = "0";
+}
